49d2ff1ee2c2fd30bc701a061ebb0e2c5eeda55b
kotwhisky <71153885+kotwhisky@users.noreply.github.com>
2020-09-12 00:12:05 +0300

    Add files via upload

---
+++ maketthird/js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+  $('.burger').click(function(event) {
+    $('.burger, .header-menu') .toggleClass('active');
+    $('body') .toggleClass('lock');
+  });
+});
+$(window).scroll(testScroll);
+var viewed = false;
+
+function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+function testScroll() {
+  if (isScrolledIntoView($(".counters")) && !viewed) {
+      viewed = true;
+      $('.count').each(function () {
+      $(this).prop('Counter',0).animate({
+          Counter: $(this).text()
+      }, {
+          duration: 2000,
+          easing: 'swing',
+          step: function (now) {
+              $(this).text(Math.ceil(now));
+          }
+      });
+    });
+  }
+}
+
